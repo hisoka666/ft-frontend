ce010e1c5a2983715d08d73f62fc1dffc6a9dcf0
Surya Sedana <pale.deathscythe@gmail.com>
2017-03-28 12:41:05 +0800

    frontend google sign in complete, next: sending authorization to server

--- front.go
+++ front.go
@@ -1,17 +1,33 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
 )
 
 func main() {
+	// variable fs membuat folder "script" menjadi sebuah file server,
+	// alamat dari file server ini akan diarahkan oleh http.Handle
+	// yang akan mengedit semua alamat URL dengan "/script/" menggunakan
+	// StripPrefix
+	// StripPrefix akan menghapus semua prefix yang berisi "/script/"
+	// dan diarahkan ke fs
+	fs := http.FileServer(http.Dir("script"))
+	http.Handle("/script/", http.StripPrefix("/script/", fs))
+
 	http.HandleFunc("/", index)
-	http.Handle("/script/", http.StripPrefix("/script/", http.FileServer(http.Dir("/script"))))
 	http.ListenAndServe(":9090", nil)
 	log.Println("Listening...")
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "index.html")
+	tmpl, err := template.New("index.html").ParseFiles("index.html")
+	if err != nil {
+		log.Fatalf("Failed to ParseFile: %v", err)
+	}
+	err = tmpl.Execute(w, nil)
+	if err != nil {
+		log.Fatalf("Failed to Execute template: %v", err)
+	}
 }
