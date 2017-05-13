ecaa351ddc7421dc88c8c37c04e372dfb0eba501
Surya Sedana <pale.deathscythe@gmail.com>
2017-05-13 13:44:04 +0800

    ubah kredensial dan lain lain

--- script/googlescript.js
+++ script/googlescript.js
@@ -1,7 +1,7 @@
 function onSignIn(googleUser) {
   var id_token = googleUser.getAuthResponse().id_token;
   $.ajax({
-    url: 'http://localhost:8080/test',
+    url: 'http://2.igdsanglah.appspot.com/test',
     type: 'post',
     data: {idtoken : id_token},
     success : function(data) {
